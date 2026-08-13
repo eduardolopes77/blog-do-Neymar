1f1e3507875f48fd00d1ccc710cd967a88143531
eduardolopes77 <eduardo.lima.santos28@escola.pr.gov.br>
2026-08-13 09:55:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const botoes = document.querySelectorAll("button");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        console.log("Reação adicionada ao post do Eduardo!");
+        let texto = botao.querySelector("span");
+        
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+            botao.style.opacity = "0.7";
+        } else {
+            texto.textContent--;
+            curtiu = false;
+            botao.style.opacity = "1";
+        }
+    }
+})
